d61ca9025718e8a628dda5b5cd53b9a2d2c79e08
Carlos Brandt <chbrandt@vo.bsdc>
2017-06-07 15:50:45 +0200

    Add env setup file

---
+++ env.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+_CURDIR=$(dirname ${BASH_SOURCE[0]})
+
+export VERITAS_REPO="${_CURDIR}"
+export VERITAS_REPO_DATA="${VERITAS_REPO}/data"
+export VERTIAS_REPO_DATA_PUB="${VERITAS_REPO_DATA}/pub"
+export VERTIAS_REPO_DATA_SRC="${VERITAS_REPO_DATA}/src"
+
