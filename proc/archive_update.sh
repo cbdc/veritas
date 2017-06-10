bc8949d71b68ff504056c80973a0a0d33aee5319
Carlos Brandt <chbrandt@workland>
2017-06-10 17:13:23 +0200

    [auto] Fix hidden files verification bug

--- proc/archive_update.sh
+++ proc/archive_update.sh
@@ -35,7 +35,7 @@ EVENT="$1"
 FILE="$2"
 DIR="$3"
 
-[ "$FILE" != ".?*" ]  || { 1>&2 echo "Not a file of interest"; exit 1; }
+[[ "$FILE" != .?* ]]  || { 1>&2 echo "Not a file of interest"; exit 1; }
 
 # Load functions
 source "${BASH_SOURCE%/*}/archive_update.fsh"
