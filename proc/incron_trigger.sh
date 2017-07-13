877746c7c6936b911cf9a19ba4db70a7bc61afe3
Carlos Brandt <chbrandt@workland>
2017-07-13 13:07:36 +0200

    Change the logfilename from incron call

--- proc/incron_trigger.sh
+++ proc/incron_trigger.sh
@@ -1,16 +1,34 @@
 #!/usr/bin/env bash
+
+# This for is to avoid triggerings from hidden ('.*') files
 for arg in "$@"
 do
     [[ "$arg" != .?* ]] || exit 1
 done
 
-CURDIR=$(cd `dirname $BASH_SOURCE`; pwd)
-LOG="${CURDIR}/veritas_incron.log"
+# The arguments are comming from incron; they go as:
+# Triggering event: $1
+# File name: $2
+# Calling directory: $3
 
-echo '----------------------------------------------------------------------' >> $LOG
-date >> $LOG
-export PATH="/opt/anaconda/bin:$PATH"
 source ~/vo.rc
-bash -x -l $REPO_VERITAS_PROC/archive_update.sh "$@"  &>> $LOG
-echo '----------------------------------------------------------------------' >> $LOG
 
+CURDIR=$(cd `dirname $BASH_SOURCE`; pwd)
+FC=$(echo `basename "$2"` | tr -d "[:space:]")
+LOGFILE="${CURDIR}/incron_veritas_${FC}.log"
+unset FC
+unset CURDIR
+
+# To avoid concurrence (specially when commit/fetching git)
+# we'll place a short random sleep before proceeding
+WAIT=$(echo "scale=2 ; 3*$RANDOM/32768" | bc -l)
+WAIT=$(echo "scale=2 ; ${WAIT}*${WAIT}" | bc -l)
+sleep "$WAIT"s
+unset WAIT
+
+echo '-----------------------------------------------------' >> $LOGFILE
+date >> $LOGFILE
+#TODO: Use a non-hardcoded Anaconda load (e.g, Environment Modules)
+export PATH="/opt/anaconda/bin:$PATH"
+bash -x -l $REPO_VERITAS_PROC/archive_update.sh "$@"  &>> $LOGFILE
+echo '-----------------------------------------------------' >> $LOGFILE
